7a0a3dbeac4de35c7353e25dca321cf8ccabb2ca
Grigori Vladimirov <grin_16@mail.ru>
2022-04-03 21:25:03 +0400

    исправления - 1 итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,32 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = popup.querySelector('.popup__close-button');
+const popupForm = document.querySelector('.popup__form');
+const popupNameInput = document.querySelector('.popup__input_el_firstname');
+const popupJobInput = document.querySelector('.popup__input_el_job');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  popupNameInput.value = profileTitle.textContent;
+  popupJobInput.value = profileSubtitle.textContent;
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
-
-
-const popupForm = document.querySelector('.popup__form');
-const popupNameInput = document.querySelector('.popup__name-input');
-const popupJobInput = document.querySelector('.popup__job-input');
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
   const valueNameInput = popupNameInput.value;
   const valueJobInput = popupJobInput.value;
-  const profileTitle = document.querySelector('.profile__title');
-  const profileSubtitle = document.querySelector('.profile__subtitle');
+  
   profileTitle.textContent = valueNameInput;
   profileSubtitle.textContent =  valueJobInput;
   closePopup();
 }
 
 popupForm.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
